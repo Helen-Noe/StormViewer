47a28b96671a5038718c31646e23363549289db1
Helen-Noe <helenmyatnoe@gmail.com>
2021-05-03 22:29:28 +1000

    table cell created

--- StromViewer/StormViewerVC.swift
+++ StromViewer/StormViewerVC.swift
@@ -8,21 +8,57 @@
 import Foundation
 import UIKit
 
-class StromViewerVC: UIViewController {
+class StromViewerVC: UIViewController{
+	
+	let tableView = UITableView()
+	var safeArea: UILayoutGuide!
+	var picturesName = [String]()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		title  = "Strom Viewer"
+		
 		view.backgroundColor = .white
-		let layout = view.layoutMarginsGuide
+		safeArea = view.layoutMarginsGuide
 		
+		let fm = FileManager.default
+		let path = Bundle.main.resourcePath!
+		let items = try! fm.contentsOfDirectory(atPath: path)
 		
-		let textLabel = UILabel()
-		textLabel.text = "Hello"
-//		textLabel.textColor = .red
-		view.addSubview(textLabel)
-		textLabel.translatesAutoresizingMaskIntoConstraints = false
-		textLabel.topAnchor.constraint(equalTo: layout.topAnchor).isActive = true
-		textLabel.leadingAnchor.constraint(equalTo: layout.leadingAnchor).isActive = true
+		for item in items{
+			if item.hasPrefix("nssl"){
+				picturesName.append(item)
+			}
+		}
+//		print(picturesName)
+		tableView.dataSource = self
+		
+		tableView.tableFooterView = UIView()
+		setupTableView()
+	}
+	
+	func setupTableView(){
+		view.addSubview(tableView)
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+		tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+		
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+	}
+}
+
+extension StromViewerVC: UITableViewDelegate,UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	return picturesName.count
+  }
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+	let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+	cell.textLabel?.text = picturesName[indexPath.row]
+	return cell
+  }
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		<#code#>
 	}
 }
